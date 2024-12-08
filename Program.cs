5600c4f95f227ed8b130a8ced2cece0fee17df45
PASSWOED <PASSWOED@DESKTOP-UI80RJC>
2024-12-08 17:33:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp112
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Check Positive OR Negative
+            Console.WriteLine("Enter The Number : ");
+            float number = Convert.ToSingle(Console.ReadLine());
+            if (number < 0)
+            {
+                Console.WriteLine("The Number Is Negative");
+            }
+            else if(number > 0)
+                {
+                Console.WriteLine("The Number Is Positive");
+                }
+            else
+            {
+                Console.WriteLine("The Number Is Zero");
+            }
+                #endregion
+            }
+        }
+    }
